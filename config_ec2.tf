de2976ae5ecab9fda1e8e18dfc42db8b199d1272
gabccardoso <54039952+gabccardoso@users.noreply.github.com>
2024-03-19 11:12:14 -0300

    Update config_ec2.tf

--- config_ec2.tf
+++ config_ec2.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  token = ${{ secrets.AWS_SESSION_TOKEN }}
 }
 
 resource "aws_security_group" "my_sg" {
