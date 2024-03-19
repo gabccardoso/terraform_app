7927d80d51441dd638837c5061321cc9b26c84be
gabccardoso <54039952+gabccardoso@users.noreply.github.com>
2024-03-19 11:14:30 -0300

    Update config_ec2.tf

--- config_ec2.tf
+++ config_ec2.tf
@@ -1,8 +1,12 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_session_token" {}
+
 provider "aws" {
   region = "us-east-1"
-  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
-  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-  token = ${{ secrets.AWS_SESSION_TOKEN }}
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  token = var.aws_session_token
 }
 
 resource "aws_security_group" "my_sg" {
